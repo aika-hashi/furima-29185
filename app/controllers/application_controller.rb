69ba44dfc93f4c3fa0d9fc91f4e21998de351cdb
aika-hashi <yu_akiramenai542_0224@yahoo.co.jp>
2020-09-01 21:18:13 +0900

    usersテーブルの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
+  
   before_action :basic_auth
+  
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
    username == 'aika' && password == '1924'
  
-    end
+  
   end
 end
