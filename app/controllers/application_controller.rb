cca62aaa192f5101d4c1c640da51a8e46da521ae
aika-hashi <yu_akiramenai542_0224@yahoo.co.jp>
2020-09-02 10:56:03 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
-  
+  before_action :authenticate_user!
   before_action :basic_auth
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
    username == 'aika' && password == '1924'
- 
-  
   end
 end
