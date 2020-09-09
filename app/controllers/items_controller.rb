fd1c7bad065c41cd05542d9b507bafe295a7bf37
aika-hashi <yu_akiramenai542_0224@yahoo.co.jp>
2020-09-09 18:50:20 +0900

    before_actionにて共通メソッド呼び出し

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only: [:show,:edit,:update]
 
   def index
    @items = Item.all.order("created_at DESC")
@@ -21,18 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
-  
   end
 
   def edit
-    @item = Item.find(params[:id])
-   
   end
     
   def update
-    @item = Item.find(params[:id])
-    
       if @item.update(item_params)
         redirect_to item_path(@item)
       else
@@ -47,6 +42,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image,:title,:text,:category_id, :condition_id, :charge_id, :area_id, :day_id, :price, :fee, :profit).merge(user_id: current_user.id)
+  end
+  
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 
