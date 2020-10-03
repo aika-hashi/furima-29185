cbf40b32bab742f9053570f5c12c52e258acba1d
aika-hashi <yu_akiramenai542_0224@yahoo.co.jp>
2020-10-03 21:15:35 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root  "items#index"
+
+  #商品詳細ページ
+  get    '/items/:id',   to:  'items#show'
+
    
   resources :items do
    resources :orders 
