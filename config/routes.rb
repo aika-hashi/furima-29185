8d684de0a0f1bc47e8b0a74439d950a74034afcd
aika-hashi <yu_akiramenai542_0224@yahoo.co.jp>
2020-10-03 21:07:55 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "items#index"
+  root  "items#index"
    
   resources :items do
    resources :orders 
